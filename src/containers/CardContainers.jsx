5bc3b36e09ac7bd8d8b54cd1498156080f541ca9
Fabricio Borgobello <fborgobello@gmail.com>
2021-09-15 12:33:21 -0300

    GET y POST requests

--- src/containers/CardContainers.jsx
+++ src/containers/CardContainers.jsx
@@ -2,31 +2,60 @@ import * as React from "react";
 import Card from "../components/Card/Card";
 
 const CardContainers = () => {
-  const [items, setItems] = React.useState([]);
-  const [cargando, setCargando] = React.useState(false);
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
 
   React.useEffect(() => {
-    setCargando(true);
-    getProducts()
-      .then((result) => setItems(result))
-      .finally(() => setCargando(false));
+    const url = "http://localhost:3001/products";
+
+    setLoading(true);
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw response;
+        }
+      })
+      .then((data) => setData(data))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
   }, []);
 
   const comprarProducto = (product) => {
     console.log(`Has comprado el producto: ${product}`);
   };
 
-  const getProducts = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(productos), 2000);
+  const postProduct = async () => {
+    const newProduct = {
+      title: "Autito",
+      description: "Esta es la descripción del producto autito",
+      price: 10008,
+      image: "https://fotos.perfil.com/2020/01/08/con-el-vision-s-sony-incursiona-en-el-mundo-de-los-autos-853254.jpg",
+    };
+
+    const response = await fetch("http://localhost:3001/products", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newProduct),
     });
+
+    return response.json();
   };
 
   return (
-    <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-      {cargando && <p style={{ fontSize: "2rem", margin: "2rem 0" }}>Cargando....</p>}
-      {!cargando &&
-        items?.map((producto) => {
+    <>
+      <button onClick={postProduct}>Cargar nuevo producto</button>
+      <div style={{ display: "flex", justifyContent: "space-evenly", flexWrap: "wrap" }}>
+        {loading && <p>Cargando...</p>}
+        {error && (
+          <p>
+            Ha habido un error: {error.status} {error.statusText}
+          </p>
+        )}
+
+        {data?.map((producto) => {
           return (
             <Card
               key={producto.id}
@@ -38,67 +67,9 @@ const CardContainers = () => {
             />
           );
         })}
-    </div>
+      </div>
+    </>
   );
 };
 
 export default CardContainers;
-
-const productos = [
-  {
-    id: 0,
-    title: "Mochila",
-    description: "Esta es la descripción del producto 1",
-    price: 10000,
-    image: "https://cdn1.static-tgdp.com/ui/productimages/approved/std.lang.all/62/64/696264_sized_1800x1200_rev_1.jpg",
-  },
-  {
-    id: 1,
-    title: "Zapatillas",
-    description: "Esta es la descripción del producto 2",
-    price: 10001,
-    image: "https://http2.mlstatic.com/D_NQ_NP_673060-MLA41186638161_032020-O.webp",
-  },
-  {
-    id: 2,
-    title: "Sombrilla",
-    description: "Esta es la descripción del producto 3",
-    price: 10002,
-    image: "https://http2.mlstatic.com/D_NQ_NP_633602-MLA44808294705_022021-O.webp",
-  },
-  {
-    id: 3,
-    title: "Remera",
-    description: "Esta es la descripción del producto 4",
-    price: 10003,
-    image: "https://http2.mlstatic.com/D_NQ_NP_877114-MLA31355994256_072019-O.webp",
-  },
-  {
-    id: 4,
-    title: "Bicicleta",
-    description: "Esta es la descripción del producto 5",
-    price: 10004,
-    image: "https://http2.mlstatic.com/D_Q_NP_799178-MLA45120499765_032021-AB.webp",
-  },
-  {
-    id: 5,
-    title: "Laptop",
-    description: "Esta es la descripción del producto 6",
-    price: 10005,
-    image: "https://http2.mlstatic.com/D_Q_NP_613206-MLA44385042469_122020-AB.webp",
-  },
-  {
-    id: 6,
-    title: "Colchón",
-    description: "Esta es la descripción del producto 7",
-    price: 10006,
-    image: "https://http2.mlstatic.com/D_NQ_NP_929454-MLA31042393012_062019-O.webp",
-  },
-  {
-    id: 7,
-    title: "Smartphone",
-    description: "Esta es la descripción del producto 8",
-    price: 10007,
-    image: "https://http2.mlstatic.com/D_Q_NP_715512-MLA46711394228_072021-AB.webp",
-  },
-];
